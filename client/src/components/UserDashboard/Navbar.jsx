dcfb9d8887f9707c3de17dbe26174392339b4374
Saanika Patel <saanikapatel07@gmail.com>
2024-07-23 11:05:18 +0530

    user dashboard corrected

--- client/src/components/UserDashboard/Navbar.jsx
+++ client/src/components/UserDashboard/Navbar.jsx
@@ -1,12 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import './userDashboard.css';
 import { StoreContext } from "../../context/StoreContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { IoHome } from "react-icons/io5";
+import { BiSolidDonateHeart } from "react-icons/bi";
 
-const Navbar = ({ onSelectOption }) => {
+const Navbar = ({ onSelectOption, removeNav }) => {
 
   const {token, setToken}=useContext(StoreContext);
   const navigate=useNavigate();
+  const [selected, setSelected] = useState("myDonations");
+
+
+  const handleOptionClick = (option) => {
+    onSelectOption(option);
+    setSelected(option);
+    removeNav(); // Close the navbar when an option is clicked
+  };
 
   const logout=()=>{
     localStorage.removeItem("token")
@@ -16,15 +26,23 @@ const Navbar = ({ onSelectOption }) => {
  
   return (
     <nav className="dashboard-navbar">
-      <ul>
-        <li>
-          <button onClick={() => onSelectOption("myDonations")}>My Donations</button>
+      <ul className="user-options">
+        <li className="nav-tag"><h1><BiSolidDonateHeart className="nav-tag-icon" />Gener<span>us</span></h1></li>
+        <li className="back-home">
+          <button><Link to="/" className="home-icon">
+            <h1><IoHome className="icon" />Home</h1>
+          </Link></button>
         </li>
         <li>
-          <button onClick={() => onSelectOption("editProfile")}>Edit Profile</button>
+          <button className={selected === "myDonations" ? "active": ""} 
+          onClick={() => handleOptionClick("myDonations")}>My Donations</button>
+        </li>
+        <li>
+          <button className={selected === "editProfile" ? "active": ""}
+          onClick={() => handleOptionClick("editProfile")}>Edit Profile</button>
         </li>
         <li onClick={logout}>
-          <button onClick={() => onSelectOption("logout")} className="logout-button">Logout</button>
+          <button onClick={() => handleOptionClick("logout")} className="logout-button">Logout</button>
         </li> 
       </ul>
     </nav>
